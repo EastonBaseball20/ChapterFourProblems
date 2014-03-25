09ba8c01626b4f1247d1d7444e797a610a94f681
Liquid-610 <jamesbarnes71@gmail.com>
2014-03-25 09:29:35 -0400

    sum is done

--- sum/sum/Program.cs
+++ sum/sum/Program.cs
@@ -9,6 +9,24 @@ namespace sum
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            int n;
+            String input;
+            while (true)
+            {
+
+                Console.WriteLine("Enter any integer value");
+                input = Console.ReadLine();
+                n = Convert.ToInt32(input);
+              
+                    sum = n + sum;
+                
+                if (input == "999")
+                {
+                    break;
+                }
+                Console.WriteLine(sum);
+            }
         }
     }
 }
