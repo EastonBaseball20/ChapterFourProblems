569b512590fa7efe1009cab9a8bc3fec3097df42
Liquid-610 <jamesbarnes71@gmail.com>
2014-03-25 09:51:17 -0400

    Even Number

---
+++ sum/EvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                for (int i = 2; i <= 100; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                
+            }
+        }
+    }
+}
