cced4bfb8c401b03dde5dd8b9954761cb4f16cbd
Liquid-610 <jamesbarnes71@gmail.com>
2014-03-25 10:13:56 -0400

    almost there

---
+++ sum/Perfect/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Perfect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                for (int n = 1; n <= i; n++)
+                {
+                    int ans = n * n;
+                    if (i == ans)
+                    {
+                        Console.WriteLine(n);
+                    }
+                }
+            }
+
+           
+        }
+    }
+}
